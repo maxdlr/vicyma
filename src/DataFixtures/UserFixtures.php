5d5dd039de41062464081a576ca058d1fa91e609
maxdlr <contact@maxdlr.com>
2024-03-20 15:02:54 +0100

    add password hashing

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -9,24 +9,37 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Exception;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture implements DependentFixtureInterface
 {
+    public function __construct(
+        public UserPasswordHasherInterface $passwordHasher
+    )
+    {
+    }
+
     /**
      * @throws Exception
      */
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create()->unique();
-
         for ($i = 0; $i < AppFixtures::USER_COUNT; $i++) {
             $user = new User();
+            $plaintextPassword = $faker->password();
+
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+
             $user
                 ->setFirstname($faker->firstName())
                 ->setLastname($faker->lastName())
                 ->setPhoneNumber($faker->phoneNumber())
                 ->setRoles([RoleEnum::ROLE_USER])
-                ->setPassword($faker->password())
+                ->setPassword($hashedPassword)
                 ->setAddress($this->getReference('address_' . rand(1, AppFixtures::ADDRESS_COUNT - 1)))
                 ->setEmail($faker->email());
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -123,6 +123,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function setPassword(string $password): static
     {
+        
+
         $this->password = $password;
 
         return $this;
