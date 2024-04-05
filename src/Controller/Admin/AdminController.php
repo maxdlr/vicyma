be03c0a05b7c58fd08b34f5a9f8ff2e00e16937e
maxdlr <contact@maxdlr.com>
2024-04-05 15:38:00 +0200

    add address form

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Lodging;
+use App\Form\AddressType;
 use App\Form\LodgingType;
+use App\Repository\AddressRepository;
 use App\Repository\LodgingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +15,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminController extends AbstractController
 {
     public function __construct(
-        private readonly LodgingRepository $lodgingRepository
+        private readonly LodgingRepository $lodgingRepository,
+        private readonly AddressRepository $addressRepository
     )
     {
     }
@@ -22,15 +25,18 @@ class AdminController extends AbstractController
     public function dashboard(): Response
     {
         $lodgings = $this->lodgingRepository->findAll();
+        $addresses = $this->addressRepository->findAll();
 //        $lodging = new Lodging();
 
 
         $lodgingForm = $this->createForm(LodgingType::class, $lodgings[0]);
+        $addressForm = $this->createForm(AddressType::class, $addresses[0]);
 
 
         return $this->render('admin/dashboard.html.twig', [
 //            'lodgings' => $lodgings,
             'lodgingForm' => $lodgingForm,
+            'addressForm' => $addressForm,
         ]);
     }
 }
