f741d0eb54f91c54878600a73696bf2a5b3095ee
maxdlr <contact@maxdlr.com>
2024-06-26 21:50:09 +0200

    set up test environment with crawler

---
+++ tests/admin/AdminDashboardTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\admin;
+
+use Symfony\Component\Panther\Client;
+use Symfony\Component\Panther\PantherTestCase;
+
+class AdminDashboardTest extends PantherTestCase
+{
+//        https://symfony.com/doc/current/testing.html#write-your-first-application-test
+    private Client $client;
+
+    public function testReservationNotificationSeeAllButton()
+    {
+        $reservationRepository = self::getContainer()->get('App\Repository\ReservationRepository');
+
+        self::assertSelectorTextContains('[data-symfony--ux-vue--vue-component-value="admin/AdminDashboard"]', 'See all');
+    }
+
+    protected function setUp(): void
+    {
+        $this->client = static::createPantherClient();
+        $this->client->request('GET', '/admin');
+        $this->client->submitForm('Sign in', ['_username' => 'contact@maxdlr.com', '_password' => 'password']);
+    }
+}
