2848b1616408520872d5ea82f9b207cd60b1c597
maxdlr <contact@maxdlr.com>
2024-06-28 10:08:19 +0200

    install chromium driver

--- tests/admin/AdminDashboardTest.php
+++ tests/admin/AdminDashboardTest.php
@@ -12,14 +12,16 @@ class AdminDashboardTest extends PantherTestCase
 
     public function testReservationNotificationSeeAllButton()
     {
-        $reservationRepository = self::getContainer()->get('App\Repository\ReservationRepository');
+//        $reservationRepository = self::getContainer()->get('App\Repository\ReservationRepository');
 
         self::assertSelectorTextContains('[data-symfony--ux-vue--vue-component-value="admin/AdminDashboard"]', 'See all');
     }
 
     protected function setUp(): void
     {
-        $this->client = static::createPantherClient();
+        $this->client = static::createPantherClient([
+            'port' => 9999
+        ]);
         $this->client->request('GET', '/admin');
         $this->client->submitForm('Sign in', ['_username' => 'contact@maxdlr.com', '_password' => 'password']);
     }
